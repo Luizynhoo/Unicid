b4f2e2a5113241043bdec98eda5424749dde7372
Luiz Henrique <115747386+Luizynhoo@users.noreply.github.com>
2024-09-04 09:28:09 -0300

    Update and rename aprendendo-C-03.c to Aprendendo-C-03.c

--- Tec.DesenvolvimentoAlgoritimo/Aprendendo-C/Aprendendo-C-03.c
+++ Tec.DesenvolvimentoAlgoritimo/Aprendendo-C/Aprendendo-C-03.c
@@ -1,15 +1,18 @@
 
 #include <stdio.h>
 
-void main() {
+//Int = numero inteiro
+int main() {
+        //Criando as variaveis
         float salario = 0;
         float aumento = 0;
-
+    
     printf("Informe o seu salario: R$");
+    //Armazenando a informação (salario) do usuario
     scanf("%f", &salario);
     printf("Informe a porcentagem de aumento do seu salario: ");
     scanf("%f", &aumento );
-     
+    //Utilizando matematica para a logica, conversão da porcentagem
     aumento = aumento*salario/100;
     salario = salario+aumento;
 
