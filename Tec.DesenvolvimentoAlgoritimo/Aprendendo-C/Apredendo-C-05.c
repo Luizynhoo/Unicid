4ad757a5138395187bf1634d654e7837eec38198
Luiz Henrique <115747386+Luizynhoo@users.noreply.github.com>
2024-09-10 19:58:45 -0300

    Atualizar o Apredendo-C-05.c

---
+++ Tec.DesenvolvimentoAlgoritimo/Aprendendo-C/Apredendo-C-05.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main ({
+    
+int x=0; 
+int Y=0;
+
+printf ("Escreva o valor de X");
+scanf ("%i", &x);
+
+printf ("Escreva o valor de Y");
+scanf ("%i", &Y);
+
+printf ("A soma dos dois valores é= %i", x+Y);
+return 0;
+}
