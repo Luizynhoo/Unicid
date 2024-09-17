a637086fdd5614fb7d5291ebe32021978c55174b
Luiz Henrique <115747386+Luizynhoo@users.noreply.github.com>
2024-09-17 19:53:28 -0300

    Create Aprendendo-C-10.c

---
+++ Tec.DesenvolvimentoAlgoritimo/Aprendendo-C/Aprendendo-C-10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+  float n1, n2, n3, n4, media;
+  printf("Digite o valor de N1: ");
+  scanf("%f", &n1);
+  printf("Digite o valor de N2: ");
+  scanf("%f", &n2);
+  printf("Digite o valor de N3: ");
+  scanf("%f", &n3);
+  printf("Digite o valor de N4: ");
+  scanf("%f", &n4);
+
+  media = (n1+n2+n3+n4)/4;
+  printf("\nA sua nota final foi: %.2f", media);
+  
+
+  return 0;
+}
+
+//Fazendo a média de um aluno
