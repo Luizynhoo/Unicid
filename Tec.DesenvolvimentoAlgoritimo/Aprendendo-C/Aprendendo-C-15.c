ffcc95df5d2499e7b7d6dcd503de24f9c4a2b89b
Luiz Henrique <115747386+Luizynhoo@users.noreply.github.com>
2024-10-05 23:28:26 -0300

    Create Aprendendo-C-15.c

---
+++ Tec.DesenvolvimentoAlgoritimo/Aprendendo-C/Aprendendo-C-15.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int a;
+    int b;
+    int count = 0;
+
+    // Lendo o valor do usuário
+    printf("Digite um valor: ");
+    scanf("%i", &b);
+
+    // Loop para imprimir os números de 'b' até 1
+    for(a = b; a > 0 && count <= 10; a--){
+        printf("%i\n", a);
+
+      count++;
+        // Verificação se o número é par ou ímpar
+        if(a % 2 == 0){
+            printf("%i é par\n", a);
+        } else {
+            printf("%i é ímpar\n", a);
+        }
+
+      
+    }
+
+    return 0;
+}
+
+//Utilizando for e if/else
