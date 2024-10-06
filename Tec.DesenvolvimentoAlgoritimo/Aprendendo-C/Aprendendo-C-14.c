97eb7b85b78cd26389f9d973f1c164d0fbd19ea3
Luiz Henrique <115747386+Luizynhoo@users.noreply.github.com>
2024-10-05 23:04:53 -0300

    Create Aprendendo-C-14.c

---
+++ Tec.DesenvolvimentoAlgoritimo/Aprendendo-C/Aprendendo-C-14.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+  
+int a;
+
+for(a = 10; a > 0; a--){
+  printf("%i\n", a);
+}
+
+return 0;
+}
+
+//Utilização do for
+//Sequencia de 10 ao 1
