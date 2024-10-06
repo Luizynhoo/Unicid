fc27d61fda3be4370e410fca2c6ec357ac1e2db4
Luiz Henrique <115747386+Luizynhoo@users.noreply.github.com>
2024-10-05 23:53:48 -0300

    Create Aprendendo-C-17.c

---
+++ Tec.DesenvolvimentoAlgoritimo/Aprendendo-C/Aprendendo-C-17.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int numero, i;
+
+    // Lendo o número do usuário
+    printf("Digite um número de 1 a 10: ");
+    scanf("%i", &numero);
+
+    // Verifica se o número está na faixa de 1 a 10
+    if (numero < 1 || numero > 10) {
+        printf("Número inválido! Por favor, digite um número de 1 a 10.\n");
+    } else {
+        // Loop para gerar a tabuada
+        printf("Tabuada do %i:\n", numero);
+        for (i = 1; i <= 10; i++) {
+            printf("%i x %i = %i\n", numero, i, numero * i);
+        }
+    }
+
+    return 0;
+}
