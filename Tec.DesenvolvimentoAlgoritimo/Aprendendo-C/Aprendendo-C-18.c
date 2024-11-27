e7740e4fe7dd06b1b8182695bdd9c695e4ceaa20
Luiz Henrique <115747386+Luizynhoo@users.noreply.github.com>
2024-11-27 00:08:19 -0300

    Create Aprendendo-C-18.c

---
+++ Tec.DesenvolvimentoAlgoritimo/Aprendendo-C/Aprendendo-C-18.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+  int i;
+  for(i = 0; i <= 20; i++){
+    if(i%2 == 0){
+      printf("%d\n", i);
+    }
+  }
+  
+
+  return 0;
+}
+
+//Escreva uma rotina que imprima os múltiplos de 2 no intervalo de 0 e 20
