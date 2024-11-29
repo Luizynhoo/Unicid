a0ae5472172219d548d6b36991892f4216018be9
Luiz Henrique <115747386+Luizynhoo@users.noreply.github.com>
2024-11-29 00:00:14 -0300

    Create Aprendendo-C-21.c

---
+++ Tec.DesenvolvimentoAlgoritimo/Aprendendo-C/Aprendendo-C-21.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+int i;
+
+for(i = 1; i<=50; i++){
+  if(i%2 != 0){
+     printf("%d\n", i);
+  }
+ 
+}
+
+
+return 0;
+
+}
+
+//Escreva uma rotina que imprima os números ímpares no intervalo de 1 a 50.
