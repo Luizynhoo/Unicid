215a85bd81d848a40c8b3292d46f4f08a5345b6b
Luiz Henrique <115747386+Luizynhoo@users.noreply.github.com>
2024-11-30 23:03:17 -0300

    Create Aprendendo-C-24.c

---
+++ Tec.DesenvolvimentoAlgoritimo/Aprendendo-C/Aprendendo-C-24.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+  int vetor[10] = {1,2,3,4,5,6,7,8,9,10};
+  int a,b;
+
+  printf("digite o valor de a e b:  ");
+  scanf("%d %d", &a,&b);
+
+  for(int i = a ; i <= b ; i++){
+
+   printf("%d\n", vetor[i]);
+
+  }
+}
+
+//vetores
+
